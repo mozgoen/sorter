ef2b9d11317a00033517099eab64e556d87ec966
Katriaryna Mazgo <ekaterina.mozgo@mail.ru>
2018-09-16 13:38:18 +0300

    sorter/fix: implemented class Sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,57 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.arrayOfElems = [];
+        this.comparator = null;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.arrayOfElems.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.arrayOfElems[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.arrayOfElems.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.arrayOfElems;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        if (indices.length === 1) {
+           return;
+        }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        indices.sort();
+
+        for (let i = 0; i < indices.length - 1; i++) {
+            for (let j = 0; j < indices.length - 1; j++) {
+                this.swap(indices[j], indices[j + 1]);
+            }
+        }
+    }
+
+    setComparator(compareFunction) {
+        this.comparator = compareFunction;
+    }
+    swap(index0, index1) {
+        if (this.comparator) {
+            if (this.comparator(this.arrayOfElems[index0], this.arrayOfElems[index1]) > 0) {
+                let swap = this.arrayOfElems[index0];
+                this.arrayOfElems[index0] = this.arrayOfElems[index1];
+                this.arrayOfElems[index1] = swap;
+            }
+        } else {
+            if (this.arrayOfElems[index0] > this.arrayOfElems[index1]) {
+                let swap = this.arrayOfElems[index0];
+                this.arrayOfElems[index0] = this.arrayOfElems[index1];
+                this.arrayOfElems[index1] = swap;
+            }
+        }
+
+    }
 }
-
 module.exports = Sorter;
